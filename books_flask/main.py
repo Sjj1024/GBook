e32262ecd8406455181c74165c1554b89ce54055
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2021-01-17 19:43:46 +0800

    添加配置类，增加配置信息

--- books_flask/main.py
+++ books_flask/main.py
@@ -1,8 +1,15 @@
 from flask import Flask
 
 
+# 集成配置类，添加到项目中
+class Config(object):
+    DEBUG = True
+
+
 app = Flask(__name__)
 
+# 项目导入配置类
+app.config.from_object(Config)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
