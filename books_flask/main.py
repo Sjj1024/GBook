349f33294cc5d169fc279bcb018344c1d576038b
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2021-01-17 20:43:15 +0800

    数据库的配置

--- books_flask/main.py
+++ books_flask/main.py
@@ -2,14 +2,22 @@ from flask import Flask
 
 
 # 集成配置类，添加到项目中
+from flask_sqlalchemy import SQLAlchemy
+
+
 class Config(object):
     DEBUG = True
-
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URL = "mysql://root:root@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICSTIONS = False
 
 app = Flask(__name__)
 
 # 项目导入配置类
 app.config.from_object(Config)
+
+# 配置数据库信息
+db = SQLAlchemy(app)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
